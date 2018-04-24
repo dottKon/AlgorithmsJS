8e6c9bf44ddcf413563c78f144f9a76d76e0e9a9
dottKon <konradhyzy@gmail.com>
2018-04-24 11:01:46 +0200

    commented version

--- linearSearch.js
+++ linearSearch.js
@@ -1,7 +1,12 @@
-function findIndex(values, target) {
-  for(var i = 0; i < values.length; ++i){
-    if (values[i] == target) { return i; }
+// Linear Search is a basic algorithm veryfying whether the given element equals to the value we are looking for. 
+
+
+// This algorithm's Big O Notation is O(N) as it would need to go through N elements of the N-long array (in the worst case scenario)
+
+function findOne(arr, num) {
+  for(var i = 0; i < arr.length; ++i){
+    if (arr[i] == num) { return i }
   }
-  return -1;
+  return false;
 }
-findIndex([7, 3, 6, 1, 0], 6)
+findOne([2, 4, 6, 8, 10], 6)
